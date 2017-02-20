5da991a605953220e65c8666a5a807206eb2d5e0
vkremneva <snowst@bk.ru>
2017-02-20 12:35:46 +0300

    DFTStraightTest: tests with constants and two sinuses added: passed

--- Test/com/sunradio/math/DFTStraightTest.groovy
+++ Test/com/sunradio/math/DFTStraightTest.groovy
@@ -6,23 +6,26 @@ class DFTStraightTest extends GroovyTestCase {
 
     final double EPS = 0.00001
     final int ITERATIONS = 100
+    final int SPLIT = 100
+    final int NUMBER = 80
 
-    // As a result of the above relation, a periodic function will contain transformed peaks in not one,
+    // As a result, a periodic function will contain transformed peaks in not one,
     // but two places. This happens because the periods of the input data become split into "positive"
     // and "negative" frequency complex components.
     // http://mathworld.wolfram.com/DiscreteFourierTransform.html
     final int meaningful = ITERATIONS/2
 
+    double[] buffer = new double[ITERATIONS]
+    double[] result = new double[ITERATIONS]
+
     //f(t) = sin(t)
     public void testSin() {
 
-        double[] buffer = new double[ITERATIONS]
-        //Split the sinus period in ITERATIONS pieces and take the sinus value in each of them
-        for (int i = 0; i < ITERATIONS; i++) buffer[i] = sin(2 * PI * i / ITERATIONS)
+        //Split the sinus period in SPLIT pieces and take the sinus value in each of them
+        for (int i = 0; i < ITERATIONS; i++)
+            buffer[i] = sin(2 * PI * i / SPLIT)
 
-        double[] result = new double[ITERATIONS]
         result = DFTStraight.run(buffer)
-
 
         int amount = 0
         for (int i = 0; i < meaningful; i++)
@@ -31,4 +34,59 @@ class DFTStraightTest extends GroovyTestCase {
 
         assertEquals(1, amount)
     }
+
+    //f(t) = NUMBER*sin(t)
+    //Test passes with any number
+    public void testConstMultSin() {
+
+        //Split the sinus period in SPLIT pieces and take the sinus value in each of them
+        for (int i = 0; i < ITERATIONS; i++)
+            buffer[i] = NUMBER * sin(2 * PI * i / SPLIT)
+
+        result = DFTStraight.run(buffer)
+
+        int amount = 0
+        for (int i = 0; i < meaningful; i++)
+            if (result[i] > EPS)
+                amount++
+
+        assertEquals(1, amount)
+    }
+
+    //f(t) = sin(NUMBER*t)
+    //Number should be less than SPLIT and more than SPLIT/2
+    //because of the sinus period
+    public void testSinMultConst() {
+
+        //Split the sinus period in SPLIT pieces and take the sinus value in each of them
+        for (int i = 0; i < ITERATIONS; i++)
+            buffer[i] = sin(NUMBER * 2 * PI * i / SPLIT)
+
+        result = DFTStraight.run(buffer)
+
+        int amount = 0
+        for (int i = 0; i < meaningful; i++)
+            if (result[i] > EPS)
+                amount++
+
+        assertEquals(1, amount)
+    }
+
+    //f(t) = NUMBER*sin(t) + sin(Number*t)
+    public void testTwoSinuses() {
+
+        //Split the sinus period in SPLIT pieces and take the sinus value in each of them
+        for (int i = 0; i < ITERATIONS; i++)
+            buffer[i] = NUMBER*sin(2 * PI * i / SPLIT) + sin(NUMBER * 2 * PI * i / SPLIT)
+
+        result = DFTStraight.run(buffer)
+
+        int amount = 0
+        for (int i = 0; i < meaningful; i++)
+            if (result[i] > EPS)
+                amount++
+
+        assertEquals(2, amount)
+    }
 }
+
