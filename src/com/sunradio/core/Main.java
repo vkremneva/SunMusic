7c6a6fe933befcee475ca1369c914c187ea29799
vkremneva <snowst@bk.ru>
2017-02-23 17:59:20 +0300

    Main file adjusted. First launch accomplished successfully. wav file in 3308 kilos processed in about 2 min

--- src/com/sunradio/core/Main.java
+++ src/com/sunradio/core/Main.java
@@ -2,6 +2,7 @@ package com.sunradio.core;
 
 import com.external.Complex;
 import com.external.WavFile;
+import com.sunradio.math.AM;
 import com.sunradio.math.DFTInverse;
 import com.sunradio.math.DFTStraight;
 
@@ -15,26 +16,27 @@ public class Main {
     public static void main(String[] args) {
         try {
             final int FRAMES = 100;
-            WavFile wavInput = WavFile.openWavFile(new File("")); //TODO: add path
+            WavFile wavInput = WavFile.openWavFile(new File("C:\\Users\\Merveilleuse\\IdeaProjects\\SunRadio\\bells.wav"));
 
-            WavFile wavOutput = WavFile.newWavFile(new File(""), //TODO: add path
+            WavFile wavOutput = WavFile.newWavFile(new File("C:\\Users\\Merveilleuse\\IdeaProjects\\SunRadio\\new.wav"),
                     wavInput.getNumChannels(), wavInput.getNumFrames(),
                     wavInput.getValidBits(), wavInput.getSampleRate());
 
             int numChannels = wavInput.getNumChannels();
             int indAmount = FRAMES * numChannels;
             double[] buffer = new double[indAmount];
-            double[] lightLevel = new double[indAmount];
+            double[] lightLevel, modulated;
 
             int frames_read;
             Complex[] transformedByDFT;
-            transformedByDFT = new Complex[indAmount];
             do {
                 frames_read = wavInput.readFrames(buffer, FRAMES);
+
                 transformedByDFT = DFTStraight.run(buffer);
                 lightLevel = LightLevel.getLightLevel(indAmount);
-                //TODO: form a filter due to light level
-                //TODO: apply filter
+
+                modulated = AM.modulate(DFTStraight.getAmplitudes(transformedByDFT), lightLevel);
+                transformedByDFT = AM.applyModulationToComplex(transformedByDFT, modulated);
 
                 buffer = DFTInverse.run(transformedByDFT);
 
